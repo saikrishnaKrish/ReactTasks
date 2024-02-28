57528d0088b6c8d0daaf1ba0944fa1bffeb52cee
Sai Krishna Kanteti <saikrishna.kanteti@gmail.com>
2024-02-29 01:19:21 +0530

    updated router for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import TasksComponent from './components/CRUD TASKS'
-import UserDetails from './components/UserDetails'
 import AppRoutes from './Routes/AppRoutes'
 
 function App() {
